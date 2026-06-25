46acedb6051f8e0039018b7a27c54c921da43527
kubilaydijital <baykankubilay@gmail.com>
2026-06-25 23:30:59 +0300

    Create About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,6 @@
+import Layout from '../components/Layout.jsx'
+import teamImage from '../assets/about-team.png'
+
+export default function About() {
+  return <Layout><main className="page"><section className="container about-grid"><div><span className="page-no">04</span><h1>Hizmet sektöründeki işletmeler için geliştirildi.</h1><p>Müşteri Takip, randevu bazlı çalışan işletmelerin kağıt defter, dağınık not ve unutulan müşteri sorunundan doğdu. Amacımız; işletmelerin müşteri sürecini tek ekranda görmesini, potansiyel müşterileri zamanında takip etmesini ve kaçan fırsatları geri kazanmasını sağlamak.</p><div className="about-values"><span>Kolay kullanım</span><span>Güvenli altyapı</span><span>Sürekli gelişim</span></div></div><div className="about-image"><img src={teamImage} alt="Müşteri Takip ekibi" /></div></section><section className="container stats-row"><div><b>1000+</b><span>Aktif işletme hedefi</span></div><div><b>%98</b><span>Müşteri memnuniyeti odağı</span></div><div><b>7/24</b><span>Erişilebilir sistem</span></div></section></main></Layout>
+}
