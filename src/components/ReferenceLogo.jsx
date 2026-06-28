c2be5dd9568910138acc2e4c4a656af74f3402d9
kubilaydijital <baykankubilay@gmail.com>
2026-06-28 23:35:18 +0300

    Update ReferenceLogo.jsx

--- src/components/ReferenceLogo.jsx
+++ src/components/ReferenceLogo.jsx
@@ -1,11 +1,6 @@
 export default function ReferenceLogo({ item, large = false }) {
-  const Icon = item.icon
-  const boxSize = large ? 64 : 52
   return (
     <div className={large ? 'reference-logo large' : 'reference-logo'}>
-      <span className="reference-icon-box" style={{ width: boxSize, height: boxSize }}>
-        <Icon size={large ? 32 : 26} />
-      </span>
       <div><strong>{item.name}</strong><small>{item.sub}</small></div>
     </div>
   )
