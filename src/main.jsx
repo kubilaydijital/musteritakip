d05ead414b2a7b9cdeab7793f117fee5bbade631
kubilaydijital <baykankubilay@gmail.com>
2026-07-13 23:24:40 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import BlogPost from './pages/BlogPost.jsx'
 import Contact from './pages/Contact.jsx'
 import Trial from './pages/Trial.jsx'
 import BookingPage from './pages/BookingPage.jsx'
+import GizlilikPolitikasi from './pages/GizlilikPolitikasi.jsx'
 import { PanelApp } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -28,6 +29,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/iletisim" element={<Contact />} />
         <Route path="/deneme" element={<Trial />} />
         <Route path="/randevu/:branchId" element={<BookingPage />} />
+        <Route path="/gizlilik-politikasi" element={<GizlilikPolitikasi />} />
         <Route path="/giris" element={<PanelApp />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
